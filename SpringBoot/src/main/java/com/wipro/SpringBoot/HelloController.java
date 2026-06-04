c4bafaccf697df34d097c5cc76d0d016572299e4
rahul <rahul@Rahul>
2026-06-04 08:24:22 +0530

    First Commit

---
+++ SpringBoot/src/main/java/com/wipro/SpringBoot/HelloController.java
@@ -0,0 +1,16 @@
+package com.wipro.SpringBoot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/message")
+	public String getMessage(Model obj) {
+		obj.addAttribute("msg", "welcome!");
+		return "home";
+		
+	}
+}
